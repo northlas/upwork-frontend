d505b0b90839396f52410632344bd8ed49a9f24b
NorthLas <liern53@gmail.com>
2025-05-12 16:02:10 +0700

    Removing context menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,7 +21,7 @@ export class AppComponent {
   title = 'upwork-frontend'
   private left?: number;
   private right?: number;
-  private selectEvent!: HTMLTextAreaElement;
+  private selectEvent?: HTMLTextAreaElement;
 
   constructor(private restService: RestService) {}
 
@@ -35,20 +35,23 @@ export class AppComponent {
   public showContext(event: MouseEvent) {
     this.left = event.clientX;
     this.right = event.clientY;
+    if (this.selectEvent == undefined) {
+      this.popover.nativeElement.style.display = 'none';
+    }
   }
 
   public onParaphrase() {
-    let value = this.selectEvent.value.substring(this.selectEvent.selectionStart, this.selectEvent.selectionEnd);
+    let value = this.selectEvent!.value.substring(this.selectEvent!.selectionStart, this.selectEvent!.selectionEnd);
     this.restService.postParapharse(value).subscribe({
       next: (response: String) => {
-        console.log(response)
-        this.selectEvent.value = this.selectEvent.value.substring(0, this.selectEvent.selectionStart) + response + this.selectEvent.value.substring(this.selectEvent.selectionEnd);
+        this.selectEvent!.value = this.selectEvent!.value.substring(0, this.selectEvent!.selectionStart) + response + this.selectEvent!.value.substring(this.selectEvent!.selectionEnd);
       },
       error: (error) => {
         console.error('Error:', error);
       },
       complete: () => {
         this.popover.nativeElement.style.display = 'none';
+        this.selectEvent = undefined;
       }
     })
   }
